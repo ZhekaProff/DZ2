7198fb51dfe72ac13b33f7b323b3ad5d13bd75a5
ZhekaProff <Zhekakad@ya.ru>
2022-05-15 11:39:01 +0900

    рушение первой задачи

---
+++ Zadacha01/Program.cs
@@ -0,0 +1,15 @@
+﻿// Дано число обозначающее день недели. Выяснить является номер дня недели выходным
+Console.WriteLine("Введите число: ");
+int count = int.Parse(Console.ReadLine());
+if (count > 0 && count < 6)
+{
+    Console.WriteLine("Число "+count+" соотвествует буднему дню недели");
+}
+else if ( count > 5 && count < 8)
+{
+    Console.WriteLine("Число "+count+" соотвествует выходному дню недели");
+}
+else
+{
+    Console.WriteLine("Число "+count+" не соотвествует не одному дню недели");
+}
