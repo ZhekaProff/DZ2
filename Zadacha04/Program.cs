1d8608adfd8eaa4abfb5feaab5f3254958ecc8d3
ZhekaProff <Zhekakad@ya.ru>
2022-05-15 13:49:05 +0900

    решение четвертой задачи

---
+++ Zadacha04/Program.cs
@@ -0,0 +1,13 @@
+﻿// Найти расстояние между точками в пространстве 2D/3D
+Console.WriteLine("Введите координаты точки А");
+Console.Write("Xa = ");
+int xa = int.Parse(Console.ReadLine());
+Console.Write("Ya = ");
+int ya = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите координаты точки B");
+Console.Write("Xb = ");
+int xb = int.Parse(Console.ReadLine());
+Console.Write("Yb = ");
+int yb = int.Parse(Console.ReadLine());
+
+Console.WriteLine(Math.Sqrt((xb-xa)*2 + (yb-ya)*2)); // для 3D нужно просто добавить координаты Z и добавить в формулу ((zb - za) *2)
